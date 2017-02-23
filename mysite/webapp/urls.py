2917cf0a5c362fbac13dc5d50a7d847b67155c1a
Anurag <chatrasen@gmail.com>
2017-02-24 02:01:55 +0530

    Added the folder mysite

---
+++ mysite/webapp/urls.py
@@ -0,0 +1,4 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [url(r'^$',views.login, name = 'login'),url(r'^register',views.register, name = 'register')]
---
+++ mysite/webapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def login(request):
+	return render(request,'webapp/home.html')
+
+def register(request):
+	return render(request,'webapp/register.html')
+
